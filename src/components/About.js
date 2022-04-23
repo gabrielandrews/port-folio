48efa75ada6ded47c7dc088c54c539f7505a174f
gabrielandrews <91432905+gabrielandrews@users.noreply.github.com>
2022-04-23 05:50:41 -0400

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -9,9 +9,9 @@ function About() {
                         alt="Error Profile" 
                         src={pic} className="picture"
                     ></img>
-            <p className="about-text">Hey! My name is Gabriel Andrews and i am a web developer who has a passion for both front and back end development. 
-            I am a graduate of the UNC coding bootcamp with a lot of new skills that im ready to use. I made this portfolio with React. If interested in my 
-            skills please feel free to contact me. 
+            <p className="about-text">Hey! My name is Gabriel Andrews and I am a web developer who has a passion for both front and back end development. 
+            I am a graduate of the UNC coding Bootcamp with a lot of new skills that I’m ready to use. I made this portfolio with React. 
+            If interested in my skills please feel free to contact me.
             </p>
         </div>
         </div>
